acb1a9268bc23e2243f97c38613dc4e68a4ff438
Your Name <you@example.com>
2017-11-03 19:53:27 +0300

    - ModelHelper instead ModelService - FormController refactoring - updateMappedRelationship method dev

---
+++ src/CoreBundle/Helper/ModelHelper.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppGear\CoreBundle\Helper;
+
+use AppGear\CoreBundle\Entity\Model;
+use AppGear\CoreBundle\Entity\Property;
+use Generator;
+
+class ModelHelper
+{
+    /**
+     * Return self and parents models
+     *
+     * @param Model $model Model
+     *
+     * @return Model[]|Generator
+     */
+    public static function getSelfAndParents(Model $model): Generator
+    {
+        do {
+            yield $model;
+        } while (null !== $model = $model->getParent());
+    }
+
+    /**
+     * Return parents models
+     *
+     * @param Model $model Model
+     *
+     * @return Model[]|Generator
+     */
+    public static function getParents(Model $model): Generator
+    {
+        while (null !== $model = $model->getParent()) {
+            yield $model;
+        }
+    }
+
+    /**
+     * Get model property by name
+     *
+     * @param Model  $model Model
+     * @param string $name  Property name
+     *
+     * @return Property|null
+     */
+    public static function getProperty(Model $model, string $name): ?Property
+    {
+        foreach (self::getProperties($model) as $property) {
+            if ($property->getName() === $name) {
+                return $property;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Get properties from model and parents
+     *
+     * @param Model $model Model
+     *
+     * @return Property[]|Generator
+     */
+    public static function getProperties(Model $model): Generator
+    {
+        foreach (self::getSelfAndParents($model) as $parent) {
+            foreach ($parent->getProperties() as $property) {
+                yield $property;
+            }
+        }
+    }
+}
